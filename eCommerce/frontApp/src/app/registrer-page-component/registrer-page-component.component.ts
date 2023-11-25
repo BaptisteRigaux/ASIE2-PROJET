81ea0b3b6695ae586493c3d240116e31cc89a638
Baptiste Rigaux <77108641+BaptisteRigaux@users.noreply.github.com>
2023-11-25 20:54:31 +0100

    Angular : confirm password registrer
    Registrer - Vérification du confirm password

--- eCommerce/frontApp/src/app/registrer-page-component/registrer-page-component.component.ts
+++ eCommerce/frontApp/src/app/registrer-page-component/registrer-page-component.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { passwordMatchValidator } from '../password-match/password-match.directives';
 
 @Component({
   selector: 'app-registrer-page-component',
@@ -7,11 +9,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./registrer-page-component.component.css']
 })
 export class RegistrerPageComponentComponent {
-  constructor(private router: Router) {}
+
+  RegistrerForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['',Validators.required]
+  },{
+    validators: passwordMatchValidator
+  })
+
+  constructor(private router: Router, private fb: FormBuilder) {}
 
   redirectToLogin() {
     console.log('Redirecting to registration page');
     this.router.navigateByUrl('/login');
   }
+
+  // Définition des inputs à utilisé pour les conditions
+  get email() { return this.RegistrerForm.controls["email"];}
+  get password() { return this.RegistrerForm.controls["password"];}
+  get confirmPassword() { return this.RegistrerForm.controls["confirmPassword"];}
 
 }
