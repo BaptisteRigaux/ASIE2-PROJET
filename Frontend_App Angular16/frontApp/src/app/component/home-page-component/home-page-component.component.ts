9e37ae334f5079f66eaf3b54ec7c5723c17b9757
Baptiste Rigaux <77108641+BaptisteRigaux@users.noreply.github.com>
2023-11-26 16:03:39 +0100

    Show all informations from connected people
    This commit can show all informations from connected people on the homepage

--- Frontend_App Angular16/frontApp/src/app/component/home-page-component/home-page-component.component.ts
+++ Frontend_App Angular16/frontApp/src/app/component/home-page-component/home-page-component.component.ts
@@ -1,4 +1,5 @@
 import { Component , OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home-page-component',
@@ -9,8 +10,25 @@ export class HomePageComponentComponent implements OnInit{
   
   userEmail: string | null = null; // Variable pour stocker l'email de l'utilisateur
 
+  userData: any; // Variable pour stocker les données utilisateur
+  
+  constructor(private http: HttpClient) { }
+
   ngOnInit() {
     // Récupérer l'email utilisateur à partir du sessionStorage
     this.userEmail = sessionStorage.getItem('email');
+
+    const userEmail = sessionStorage.getItem('email'); // Récupérer l'email de la session
+
+    if (userEmail) {
+      this.http.get(`http://localhost:8080/user/email=${userEmail}`).subscribe(
+        (data: any) => {
+          this.userData = data;
+        },
+        (error) => {
+          console.error('Erreur lors de la récupération des données utilisateur : ', error);
+        }
+      );
+    }
   }
 }
